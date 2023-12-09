560487b0068f16956a7a9574b63242ec7d82fcc3
Bryan_Jhuo <t111590038@ntut.org.tw>
2023-12-10 06:10:45 +0800

    add window & upgrading-fetch function

--- apple.py
+++ apple.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import json as js
 
 
-url = "https://www.apple.com/tw"
 url_1 = "https://www.apple.com/tw/shop/buy-iphone/iphone-15-pro"
 request = requests.get(url_1)
 html = BeautifulSoup(request.content, "html.parser")
@@ -12,34 +12,51 @@ test1 = html.find_all("span", {"class": "globalnav-link-text"})
 #清單json
 json_text = html.find("script", {"id": "__ACGH_DATA__", "type": "application/json"})
 '''
+def getColor():     #擷取顏色
+    colors = []
+    allColors = html.find("div", {"class": "product-selection-area noscript as-l-container"}).find(
+        "div").find_all("span", {"class": "dimensionColor"})
+    for single_color in allColors:
+        if single_color.contents[0] not in colors:
+            colors.append(single_color.contents[0])
+    return colors
 
-#擷取顏色
-colors = []
-allColors = html.find("div", {"class": "product-selection-area noscript as-l-container"}).find(
-    "div").find_all("span", {"class": "dimensionColor"})
-for single_color in allColors:
-    if single_color.contents[0] not in colors:
-        colors.append(single_color.contents[0])
 
-#擷取容量
-capacities = []
-allCapacity = html.find("div", {"class": "product-selection-area noscript as-l-container"}).find(
-    "div").find_all("span", {"class": "dimensionCapacity"})
+def getCapacity():    #擷取容量
+    capacities = []
+    allCapacity = html.find("div", {"class": "product-selection-area noscript as-l-container"}).find(
+        "div").find_all("span", {"class": "dimensionCapacity"})
 
-for single_capacity in allCapacity:
-    size = single_capacity.contents[0]
-    if int(size) < 10:
-        size += "TB"
-    else:
-        size += "GB"
+    for single_capacity in allCapacity:
+        size = single_capacity.contents[0]
+        if int(size) < 10:
+            size += "tb"
+        else:
+            size += "gb"
 
-    if size not in capacities:
-        capacities.append(size)
+        if size not in capacities:
+            capacities.append(size)
 
-capacities = sorted(capacities)
-for i in range(1, 3):
-    temp = capacities[i]
-    capacities[i] = capacities[i + 1]
-    capacities[i + 1] = temp
+    capacities = sorted(capacities)
+    for i in range(1, 3):
+        temp = capacities[i]
+        capacities[i] = capacities[i + 1]
+        capacities[i + 1] = temp
+    return capacities
 
-print(capacities)
+#取貨資訊在pickupDisplay  無貨:ineligible 有貨:available
+#取貨店面在storeName
+delivery_url = "https://www.apple.com/tw/shop/fulfillment-messages?little=false&parts.0=MTV93ZP/A&mts.0=regular&mts.1=sticky&fts=true"
+next_url = "https://www.apple.com/tw/shop/fulfillment-messages?little=false&parts.0=MTUX3ZP/A&mts.0=regular&mts.1=sticky&fts=true"
+
+def getDetail(size, capacity, color):   #擷取型號、價錢、產編
+    result = []
+    url = "https://www.apple.com/tw/shop/buy-iphone/iphone-15-pro/" + size + "吋顯示器-" + capacity + "-" + color
+    req = requests.get(url)
+    bsObj = BeautifulSoup(req.content, "html.parser")
+    detail = bsObj.find("script", {"type": "application/ld+json"})
+    detailJson = js.loads(detail.get_text())
+    result.append(detailJson['name'])
+    result.append(detailJson['offers'][0]['priceCurrency'] + " " + str(detailJson['offers'][0]['price']))
+    result.append(detailJson['offers'][0]['sku'])
+    return result
--- main.py
+++ main.py
@@ -4,13 +4,71 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import apple
+import tkinter
+
+def createWindow():
+    window = tkinter.Tk()
+    window.title("Mini Apple Store")
+    window.geometry('510x500')
+    window.resizable(width=False, height=False)
+    return window
+
+def createRadioButton(phoneState, colorState, capacityState):
+    colors = apple.getColor()
+    capacity = apple.getCapacity()
+    phoneButton1 = tkinter.Radiobutton(text="IPhone 15 Pro", value="6.1-", variable=phoneState)
+    phoneButton2 = tkinter.Radiobutton(text="IPhone 15 ProMax", value="6.7-", variable=phoneState)
+    colorButton1 = tkinter.Radiobutton(text=""+colors[0], value=colors[0], variable=colorState)
+    colorButton2 = tkinter.Radiobutton(text=""+colors[1], value=colors[1], variable=colorState)
+    colorButton3 = tkinter.Radiobutton(text=""+colors[2], value=colors[2], variable=colorState)
+    colorButton4 = tkinter.Radiobutton(text=""+colors[3], value=colors[3], variable=colorState)
+    capacityButton1 = tkinter.Radiobutton(text=""+capacity[0], value=capacity[0], variable=capacityState)
+    capacityButton2 = tkinter.Radiobutton(text=""+capacity[1], value=capacity[1], variable=capacityState)
+    capacityButton3 = tkinter.Radiobutton(text=""+capacity[2], value=capacity[2], variable=capacityState)
+    capacityButton4 = tkinter.Radiobutton(text=""+capacity[3], value=capacity[3], variable=capacityState)
+    phoneButton1.place(x=55, y=50)
+    phoneButton1.select()
+    phoneButton2.place(x=55, y=70)
+    colorButton1.place(x=200, y=50)
+    colorButton1.select()
+    colorButton2.place(x=200, y=70)
+    colorButton3.place(x=200, y=90)
+    colorButton4.place(x=200, y=110)
+    capacityButton1.place(x=300, y=50)
+    capacityButton1.select()
+    capacityButton2.place(x=300, y=70)
+    capacityButton3.place(x=300, y=90)
+    capacityButton4.place(x=300, y=110)
+    return
+
+def updateResult():
+    if phoneState.get() == "6.7-" and capacityState.get() == "128gb":
+        detailState.set("Error")
+    else:
+        myChoice = apple.getDetail(phoneState.get(), capacityState.get(), colorState.get())
+        detailState.set(myChoice[0] + " " + myChoice[1])
+
+if __name__ == '__main__':
+    print("Starting....")
+    mainWindow = createWindow()
+    title_Label = tkinter.Label(text="Choice Your IPhone", font=("Arial", 16, "bold"), padx=5, pady=5,
+                                bg="Black", fg="white")
+    title_Label.pack()
+
+    phoneState = tkinter.StringVar()
+    colorState = tkinter.StringVar()
+    capacityState = tkinter.StringVar()
+    detailState = tkinter.StringVar()
+    createRadioButton(phoneState, colorState, capacityState)
+    result_Label = tkinter.Label(textvariable=detailState, font=("Arial", 16, "bold"), padx=5,
+                                 pady=5, bg="white", fg="black")
+    result_Label.place(x=0, y=200)
+
+    confirmButton = tkinter.Button(text="Confirm", font=('Arial', 16, 'bold'), command=updateResult)
+    confirmButton.place(x=200, y=150)
+
+    mainWindow.mainloop()
+
